bd5c0d7d1bb04ade8c7e805db647a347813500a3
Misagh <mishaker.code@gmail.com>
2019-05-06 20:46:48 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import render_template
+import connexion
+
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir="./")
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api("swagger.yml")
+
+# If we're running in stand alone mode, run the application
+if __name__ == "__main__":
+    app.run(debug=True)
